274f7c1a0b2195d6d2a25fed489c78096226d862
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-24 13:34:31 +0100

    Add skeleton porcelain endpoint and deps

--- R/util.R
+++ R/util.R
@@ -1,3 +1,21 @@
-`%||%` <- function(x, y) { # nolint
-  if (is.null(x)) y else x
+scalar <- function(x) {
+  jsonlite::unbox(x)
+}
+
+package_version_string <- function(name) {
+  as.character(utils::packageVersion(name))
+}
+
+uglify <- function(code) {
+  ## This works for now but is slow; hopefully we can come up with
+  ## some v8-hosted solution soon.
+  code
+}
+
+prepare_code <- function(code, pretty) {
+  code <- paste(code, collapse = "\n")
+  if (!pretty) {
+    code <- uglify(code)
+  }
+  code
 }
