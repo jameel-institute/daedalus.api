80726fed1f6270199a5ef7a0e64824b795d3a5e9
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-24 13:34:31 +0100

    Add skeleton porcelain endpoint and deps

---
+++ R/api.R
@@ -0,0 +1,32 @@
+##' Create an daedalus.api server, a porcelain object
+##'
+##' @title Create daedalus.api
+##'
+##' @param validate Logical, indicating if validation should be done
+##'   on responses.  This should be `FALSE` in production
+##'   environments.  See [porcelain::porcelain] for details
+##'
+##' @param log_level Logging level to use. Sensible options are "off",
+##'   "info" and "all".
+##'
+##' @return A [porcelain::porcelain] object. Notably this does *not*
+##'   start the server
+##'
+##' @export
+api <- function(validate = NULL, log_level = "info") {
+  logger <- make_logger(log_level)
+  api <- porcelain::porcelain$new(validate = validate, logger = logger)
+  api$include_package_endpoints()
+  api
+}
+
+##' @porcelain GET / => json(root)
+root <- function() {
+  versions <- c(
+    list(
+      daedalus = utils::packageVersion("daedalus"),
+      daedalus.api = utils::packageVersion("daedalus.api")
+    )
+  )
+  lapply(versions, function(v) scalar(as.character(v)))
+}
