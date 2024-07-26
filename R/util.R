f592fa783e599f3ee7a6415eedf8de270a498c2e
Pratik Gupte <pratikgupte16@gmail.com>
2024-07-26 12:08:14 +0100

    Rm `uglify()` and `prepare_code()`

--- R/util.R
+++ R/util.R
@@ -5,17 +5,3 @@ scalar <- function(x) {
 package_version_string <- function(name) {
   as.character(utils::packageVersion(name))
 }
-
-uglify <- function(code) {
-  ## This works for now but is slow; hopefully we can come up with
-  ## some v8-hosted solution soon.
-  code
-}
-
-prepare_code <- function(code, pretty) {
-  code <- paste(code, collapse = "\n")
-  if (!pretty) {
-    code <- uglify(code)
-  }
-  code
-}
--- tests/testthat/test-util.R
+++
@@ -1,7 +0,0 @@
-test_that("Can uglify es6 code", {
-  s <- "class a { };"
-  s_ugly <- s # should be "class a{}"
-  expect_identical(uglify(s), s_ugly)
-  expect_identical(prepare_code(s, TRUE), s)
-  expect_identical(prepare_code(s, FALSE), s_ugly)
-})
