f28b0f2d872ce3dac326e2b05bab6347ac72bce5
Emma <EmmaRussellKilburn@gmail.com>
2024-09-07 14:23:23 +0100

    remove irrelevant comment

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -1,6 +1,6 @@
 test_that("can get hospital capacity for population", {
   result <- get_hospital_capacity_for_pop(1263182, 10)
-  expect_identical(result$min, 380)#
+  expect_identical(result$min, 380)
   expect_identical(result$default, 570)
   expect_identical(result$max, 1640)
 })
