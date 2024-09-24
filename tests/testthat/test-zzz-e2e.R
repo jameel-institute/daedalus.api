812dab5d2cca5738769d2ffec914fc4d48f67138
Emma <EmmaRussellKilburn@gmail.com>
2024-09-24 14:23:05 +0100

    include default tolerance

--- tests/testthat/test-zzz-e2e.R
+++ tests/testthat/test-zzz-e2e.R
@@ -87,6 +87,7 @@ test_that("can run model, get status and results", {
   expect_gt(length(results_data$time_series), 0)
 
   # 5. Test nested costs - values should add up
+  tolerance <- testthat_tolerance()
   costs_total <- results_data$costs[[1]]
   expect_identical(costs_total$id, "total")
   gdp_total <- costs_total$children[[1]]
@@ -95,23 +96,27 @@ test_that("can run model, get status and results", {
   expect_identical(education_total$id, "education")
   life_years_total <- costs_total$children[[3]]
   expect_identical(life_years_total$id, "life_years")
-  expect_identical(costs_total$value,
-               gdp_total$value + education_total$value + life_years_total$value,
-               tolerance = 0.1)
+  expect_equal(costs_total$value,
+               sum(gdp_total$value,
+                   education_total$value,
+                   life_years_total$value),
+               tolerance = tolerance)
   gdp_closures <- gdp_total$children[[1]]
   expect_identical(gdp_closures$id, "gdp_closures")
   gdp_absences <- gdp_total$children[[2]]
   expect_identical(gdp_absences$id, "gdp_absences")
-  expect_identical(gdp_total$value,
+  expect_equal(gdp_total$value,
                sum(gdp_closures$value,
-                   gdp_absences$value))
+                   gdp_absences$value),
+               tolerance = tolerance)
   education_closures <- education_total$children[[1]]
   expect_identical(education_closures$id, "education_closures")
   education_absences <- education_total$children[[2]]
   expect_identical(education_absences$id, "education_absences")
-  expect_identical(education_total$value,
+  expect_equal(education_total$value,
                sum(education_closures$value,
-                   education_absences$value))
+                   education_absences$value),
+               tolerance = tolerance)
   lifeyears_infants <- life_years_total$children[[1]]
   expect_identical(lifeyears_infants$id, "life_years_infants")
   lifeyears_adolescents <- life_years_total$children[[2]]
@@ -120,9 +125,10 @@ test_that("can run model, get status and results", {
   expect_identical(lifeyears_working_age$id, "life_years_working_age")
   lifeyears_retirement_age <- life_years_total$children[[4]]
   expect_identical(lifeyears_retirement_age$id, "life_years_retirement_age")
-  expect_identical(life_years_total$value,
+  expect_equal(life_years_total$value,
                sum(lifeyears_infants$value,
                    lifeyears_adolescents$value,
                    lifeyears_working_age$value,
-                   lifeyears_retirement_age$value))
+                   lifeyears_retirement_age$value),
+               tolerance = tolerance)
 })
