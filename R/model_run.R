7d7d610297d9cbf1bd3fca41e3b103187b57506b
Emma <EmmaRussellKilburn@gmail.com>
2024-10-21 11:32:20 +0100

    time series metadata

--- R/model_run.R
+++ R/model_run.R
@@ -43,9 +43,9 @@ model_run <- function(parameters, model_version) {
 
   # get incidence time series
   time_series$new_infections <- daedalus::get_incidence(model_results, "infections")$value
-  time_series$new_hospitalised <- daedalus::get_incidence(model_results, "hospitalisations")$value
+  time_series$new_hospitalisations <- daedalus::get_incidence(model_results, "hospitalisations")$value
   time_series$new_deaths <- daedalus::get_incidence(model_results, "deaths")$value
-  time_series$new_vaccinated <- daedalus::get_new_vaccinations(model_results)$new_vaccinations
+  time_series$new_vaccinations <- daedalus::get_new_vaccinations(model_results)$new_vaccinations
 
   raw_costs <- daedalus::get_costs(model_results)
   costs <- get_nested_costs(raw_costs)
