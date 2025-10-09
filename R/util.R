e367065acd9e12b3b92989fe1648f0835a86b5ea
Pratik Gupte <pratikgupte16@gmail.com>
2025-10-09 14:42:02 +0100

    Update fn `get_nested_costs()` to match JSON schema

--- R/util.R
+++ R/util.R
@@ -140,81 +140,83 @@ get_nested_costs <- function(raw_costs) {
 
   # NOTE: daedalus returns life years and values separately;
   # accessing value here but retaining 'years' as var name
-  life_years <- raw_costs$life_value_lost$life_value_lost_total
-  life_years_age <- raw_costs$life_value_lost$life_value_lost_age
+  life_value_total <- raw_costs$life_value_lost$life_value_lost_total
+  life_value_age <- raw_costs$life_value_lost$life_value_lost_age
 
+  life_years_data <- get_life_years_lost(raw_costs)
+  life_years_total <- life_years_data$life_years_lost_total
+  life_years_age <- life_years_data$life_years_age
+
+  # NOTE: the direct output of this function does not validate against
+  # inst/scenarioCosts.json; the exact reason for returning a list rather than a
+  # cost_item() output is unknown.
   list(
     cost_item(
       "total",
-      total,
+      list(list(metric = "usd", value = total)),
       list(
         cost_item(
           "gdp",
-          gdp,
+          list(list(metric = "usd", value = gdp)),
           list(
-            cost_item("gdp_closures", gdp_closures),
-            cost_item("gdp_absences", gdp_absences)
+            cost_item(
+              "gdp_closures",
+              list(list(metric = "usd", value = gdp_closures))
+            ),
+            cost_item(
+              "gdp_absences",
+              list(list(metric = "usd", value = gdp_absences))
+            )
           )
         ),
         cost_item(
           "education",
-          education,
+          list(list(metric = "usd", value = education)),
           list(
-            cost_item("education_closures", education_closures),
-            cost_item("education_absences", education_absences)
+            cost_item(
+              "education_closures",
+              list(list(metric = "usd", value = education_closures))
+            ),
+            cost_item(
+              "education_absences",
+              list(list(metric = "usd", value = education_absences))
+            )
           )
         ),
         cost_item(
           "life_years",
-          life_years,
           list(
-            cost_item("life_years_pre_school", life_years_age[["0-4"]]),
-            cost_item("life_years_school_age", life_years_age[["5-19"]]),
-            cost_item("life_years_working_age", life_years_age[["20-64"]]),
-            cost_item("life_years_retirement_age", life_years_age[["65+"]])
-          )
-        )
-      )
-    )
-  )
-}
-
-#' @name costs_to_display
-#'
-#' @inheritParams costs_to_display
-#'
-#' @keywords internal
-get_nested_natural_costs <- function(raw_costs) {
-  # a grand total doesn't make sense as units are incomparable
-  # all totals are for nested elements
-
-  life_years_lost <- get_life_years_lost(raw_costs)
-
-  # this list only has meaningful sub-lists, top level id and value are dummies
-  list(
-    cost_item(
-      "total",
-      NULL,
-      list(
-        cost_item(
-          "life_years",
-          life_years_lost$life_years_lost_total,
+            list(metric = "usd", value = life_value_total),
+            list(metric = "life_years", value = life_years_total)
+          ),
           list(
             cost_item(
               "life_years_pre_school",
-              life_years_lost$life_years_age[["0-4"]]
+              list(
+                list(metric = "usd", value = life_value_age[["0-4"]]),
+                list(metric = "life_years", value = life_years_age[["0-4"]])
+              )
             ),
             cost_item(
               "life_years_school_age",
-              life_years_lost$life_years_age[["5-19"]]
+              list(
+                list(metric = "usd", value = life_value_age[["5-19"]]),
+                list(metric = "life_years", value = life_years_age[["5-19"]])
+              )
             ),
             cost_item(
               "life_years_working_age",
-              life_years_lost$life_years_age[["20-64"]]
+              list(
+                list(metric = "usd", value = life_value_age[["20-64"]]),
+                list(metric = "life_years", value = life_years_age[["20-64"]])
+              )
             ),
             cost_item(
               "life_years_retirement_age",
-              life_years_lost$life_years_age[["65+"]]
+              list(
+                list(metric = "usd", value = life_value_age[["65+"]]),
+                list(metric = "life_years", value = life_years_age[["65+"]])
+              )
             )
           )
         )
@@ -298,7 +300,7 @@ get_average_vsl <- function(country) {
 #'
 #' @keywords internal
 cost_item <- function(id, value, children = NULL) {
-  item <- list(id = id, value = value)
+  item <- list(id = id, values = value)
   if (!is.null(children)) {
     item$children <- children
   }
