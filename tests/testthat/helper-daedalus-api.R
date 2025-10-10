ee658b8b6bdd5bbce74c96c35297006d47a9d479
Pratik Gupte <pratikgupte16@gmail.com>
2025-10-10 10:18:01 +0100

    Update tests helper fn expect_daedalus_mock_costs()

--- tests/testthat/helper-daedalus-api.R
+++ tests/testthat/helper-daedalus-api.R
@@ -82,65 +82,106 @@ daedalus_mock_costs <- function() {
   )
 }
 
+# A wrapper around multiple expectations for the form of the output of
+# `daedalus.api::get_nested_costs()`.
 expect_nested_mock_costs <- function(costs) {
   expect_length(costs, 1L)
   total <- costs[[1]]
   expect_identical(total$id, "total")
-  expect_identical(total$value, 100)
+  expect_identical(
+    total$value,
+    list(list(metric = "usd", value = 100))
+  )
   expect_length(total$children, 3L)
 
   gdp <- total$children[[1]]
   expect_identical(gdp$id, "gdp")
-  expect_identical(gdp$value, 20)
+  expect_identical(gdp$value, list(list(metric = "usd", value = 20)))
   expect_length(gdp$children, 2L)
 
   gdp_closures <- gdp$children[[1]]
   expect_identical(gdp_closures$id, "gdp_closures")
-  expect_identical(gdp_closures$value, 5)
+  expect_identical(gdp_closures$value, list(list(metric = "usd", value = 5)))
   expect_false("children" %in% names(gdp_closures))
 
   gdp_absences <- gdp$children[[2]]
   expect_identical(gdp_absences$id, "gdp_absences")
-  expect_identical(gdp_absences$value, 15)
+  expect_identical(gdp_absences$value, list(list(metric = "usd", value = 15)))
   expect_false("children" %in% names(gdp_absences))
 
   education <- total$children[[2]]
   expect_identical(education$id, "education")
-  expect_identical(education$value, 30)
+  expect_identical(education$value, list(list(metric = "usd", value = 30)))
   expect_length(education$children, 2L)
 
   education_closures <- education$children[[1]]
   expect_identical(education_closures$id, "education_closures")
-  expect_identical(education_closures$value, 10)
+  expect_identical(
+    education_closures$value,
+    list(list(metric = "usd", value = 10))
+  )
   expect_false("children" %in% names(education_closures))
 
   education_absences <- education$children[[2]]
   expect_identical(education_absences$id, "education_absences")
-  expect_identical(education_absences$value, 20)
+  expect_identical(
+    education_absences$value,
+    list(list(metric = "usd", value = 20))
+  )
   expect_false("children" %in% names(education_absences))
 
   life_years <- total$children[[3]]
   expect_identical(life_years$id, "life_years")
-  expect_identical(life_years$value, 50)
+  expect_identical(
+    life_years$value,
+    list(
+      list(metric = "usd", value = 50),
+      list(metric = "life_years", value = 5000)
+    )
+  )
   expect_length(life_years$children, 4L)
 
   life_years_pre_school <- life_years$children[[1]]
   expect_identical(life_years_pre_school$id, "life_years_pre_school")
-  expect_identical(life_years_pre_school$value, 5)
+  expect_identical(
+    life_years_pre_school$value,
+    list(
+      list(metric = "usd", value = 5),
+      list(metric = "life_years", value = 500)
+    )
+  )
   expect_false("children" %in% names(life_years_pre_school))
 
   life_years_school_age <- life_years$children[[2]]
   expect_identical(life_years_school_age$id, "life_years_school_age")
-  expect_identical(life_years_school_age$value, 10)
+  expect_identical(
+    life_years_school_age$value,
+    list(
+      list(metric = "usd", value = 10),
+      list(metric = "life_years", value = 1000)
+    )
+  )
   expect_false("children" %in% names(life_years_school_age))
 
   life_years_working_age <- life_years$children[[3]]
   expect_identical(life_years_working_age$id, "life_years_working_age")
-  expect_identical(life_years_working_age$value, 15)
+  expect_identical(
+    life_years_working_age$value,
+    list(
+      list(metric = "usd", value = 15),
+      list(metric = "life_years", value = 1500)
+    )
+  )
   expect_false("children" %in% names(life_years_working_age))
 
   life_years_retirement_age <- life_years$children[[4]]
   expect_identical(life_years_retirement_age$id, "life_years_retirement_age")
-  expect_identical(life_years_retirement_age$value, 20)
+  expect_identical(
+    life_years_retirement_age$value,
+    list(
+      list(metric = "usd", value = 20),
+      list(metric = "life_years", value = 2000)
+    )
+  )
   expect_false("children" %in% names(life_years_retirement_age))
 }
