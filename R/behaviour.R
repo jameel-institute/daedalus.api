c8ed5f89ded215a097da5cc958eb294472c7b860
Pratik Gupte <pratikgupte16@gmail.com>
2025-10-31 12:19:47 +0000

    Update behaviour label, desc, metadata JSON, and reverse behav option mapping

--- R/behaviour.R
+++ R/behaviour.R
@@ -1,5 +1,10 @@
 #' Translate behaviour choice to daedalus input
 #'
+#' @description
+#' Note that the mapping from the dashboard options to the optimism parameter
+#' is reversed. For a dashboard option of 'low', optimism is high. This allows
+#' the dashboard option to present as 'change in public behaviour'.
+#'
 #' @param x A single string for the behavioural choice.
 #'
 #' @param hospital_capacity The country hospital capacity.
@@ -15,15 +20,21 @@ process_behaviour_choice <- function(x, hospital_capacity) {
     none = NULL,
     low = daedalus::daedalus_new_behaviour(
       hospital_capacity,
-      baseline_optimism = 0.25
+      baseline_optimism = 0.75,
+      responsiveness = 0.01,
+      behav_effectiveness = 0.2
     ),
     medium = daedalus::daedalus_new_behaviour(
       hospital_capacity,
-      baseline_optimism = 0.5
+      baseline_optimism = 0.5,
+      responsiveness = 0.01,
+      behav_effectiveness = 0.2
     ),
     high = daedalus::daedalus_new_behaviour(
       hospital_capacity,
-      baseline_optimism = 0.75
+      baseline_optimism = 0.25,
+      responsiveness = 0.01,
+      behav_effectiveness = 0.2
     )
   )
 
